bb8464fbe809533f507cf0bc4fff688cd6ed4fc2
leo-exee <leopold@littlebill.io>
2025-04-24 15:22:17 +0200

    feat: enhance player index view with improved layout, role display, and team association

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,11 +1,28 @@
 class Player < ApplicationRecord
   belongs_to :team
 
-  ROLES = ["Top laner", "Jungler", "Mid laner", "ADC", "Support"]
+  ROLES = [ "Top laner", "Jungler", "Mid laner", "ADC", "Support" ]
 
   validates :first_name, :last_name, :role, presence: true
   validates :role, inclusion: { in: ROLES }
-  validate :team_must_have_less_than_five_players, on: [:create, :update]
+  validate :team_must_have_less_than_five_players, on: [ :create, :update ]
+
+  def role_icon
+    case role
+    when "Top laner"
+      "https://static.wikia.nocookie.net/leagueoflegends/images/0/0a/Top_Laner_icon.png"
+    when "Jungler"
+      "https://static.wikia.nocookie.net/leagueoflegends/images/1/17/Jungle_Laner_icon.png"
+    when "Mid laner"
+      "https://static.wikia.nocookie.net/leagueoflegends/images/4/45/Mid_Laner_icon.png"
+    when "ADC"
+      "https://static.wikia.nocookie.net/leagueoflegends/images/c/c6/ADC_Laner_icon.png"
+    when "Support"
+      "https://static.wikia.nocookie.net/leagueoflegends/images/6/6d/Support_Laner_icon.png"
+    else
+      "https://via.placeholder.com/150"
+    end
+  end
 
   private
 
