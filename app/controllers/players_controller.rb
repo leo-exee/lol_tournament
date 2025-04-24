82a2509f7e716124db47fbba527d120630b8e3a4
leo-exee <leopold@littlebill.io>
2025-04-24 15:28:54 +0200

    feat: enhance player management views with French localization and improved error messages

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,6 @@
 class PlayersController < ApplicationController
   before_action :set_player, only: %i[ show edit update destroy ]
-  before_action :require_admin, only: [:new, :create, :edit, :update, :destroy]
+  before_action :require_admin, only: [ :new, :create, :edit, :update, :destroy ]
 
   # GET /players or /players.json
   def index
@@ -16,10 +16,20 @@ class PlayersController < ApplicationController
     @player = Player.new
     @teams = Team.all
     @roles = Player::ROLES
+    if @teams.empty?
+      flash[:alert] = "Il n'y a pas d'équipe disponible. Veuillez créer une équipe avant d'ajouter un joueur."
+      redirect_to teams_path
+    end
   end
 
   # GET /players/1/edit
   def edit
+    @teams = Team.all
+    @roles = Player::ROLES
+    if @teams.empty?
+      flash[:alert] = "Il n'y a pas d'équipe disponible. Veuillez créer une équipe avant d'ajouter un joueur."
+      redirect_to teams_path
+    end
   end
 
   # POST /players or /players.json
@@ -30,7 +40,7 @@ class PlayersController < ApplicationController
 
     respond_to do |format|
       if @player.save
-        format.html { redirect_to @player, notice: "Player was successfully created." }
+        format.html { redirect_to @player, notice: "Le joueur a été créé avec succès." }
         format.json { render :show, status: :created, location: @player }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,43 +48,39 @@ class PlayersController < ApplicationController
       end
     end
   end
-  
 
-# PATCH/PUT /players/1 or /players/1.json
-def update
-  @player = Player.find(params[:id])
-  @teams = Team.all
-  @roles = Player::ROLES
+  # PATCH/PUT /players/1 or /players/1.json
+  def update
+    @teams = Team.all
+    @roles = Player::ROLES
 
-  respond_to do |format|
-    if @player.update(player_params)
-      format.html { redirect_to @player, notice: "Player was successfully updated." }
-      format.json { render :show, status: :ok, location: @player }
-    else
-      format.html { render :edit, status: :unprocessable_entity }
-      format.json { render json: @player.errors, status: :unprocessable_entity }
+    respond_to do |format|
+      if @player.update(player_params)
+        format.html { redirect_to @player, notice: "Le joueur a été mis à jour avec succès." }
+        format.json { render :show, status: :ok, location: @player }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @player.errors, status: :unprocessable_entity }
+      end
     end
   end
-end
 
   # DELETE /players/1 or /players/1.json
   def destroy
     @player.destroy!
 
     respond_to do |format|
-      format.html { redirect_to players_path, status: :see_other, notice: "Player was successfully destroyed." }
+      format.html { redirect_to players_path, status: :see_other, notice: "Le joueur a été supprimé avec succès." }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_player
-      @player = Player.find(params.expect(:id))
+      @player = Player.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def player_params
-      params.expect(player: [ :first_name, :last_name, :role, :team_id ])
+      params.require(:player).permit(:first_name, :last_name, :role, :team_id)
     end
 end
