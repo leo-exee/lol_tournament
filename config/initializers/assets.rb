26f0e366f280a2e3a3f627a8d4aaebecb55f4680
leo-exee <leopold@littlebill.io>
2025-04-24 13:39:17 +0200

    Implement code changes to enhance functionality and improve performance

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,12 +1,3 @@
-# Be sure to restart your server when you modify this file.
-
-# Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = "1.0"
-# config/initializers/assets.rb
-Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts", "stylesheets")
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts", "stylesheets", "images")
 Rails.application.config.assets.precompile += %w[ *.ttf *.otf ]
-
-
-
-# Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
