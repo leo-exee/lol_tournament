f52028e4f3a3f30224dde558852217689c53c081
leo-exee <leopold@littlebill.io>
2025-04-24 15:06:41 +0200

    feat: update team index view to include player count emoji and fix team_params method for strong parameters

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -65,13 +65,11 @@ class TeamsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_team
       @team = Team.find(params.expect(:id))
     end
 
-    # Only allow a list of trusted parameters through.
     def team_params
-      params.expect(team: [ :name ])
+      params.require(:team).permit(:name, :logo)
     end
 end
